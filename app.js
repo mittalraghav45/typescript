b6f0e523392412c71ab399eafa8b088963df0ed1
Raghav Mittal <mittalraghav45@gmail.com>
2025-04-14 20:43:07 +0100

    how to use private methods

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ class Users {
     }
     displayName() {
         console.log(this.name);
+        this.getNameLength();
+    }
+    getNameLength() {
+        console.log(this.name.length);
     }
 }
 const u1 = new Users();
 u1.setName("John");
+// u1.name='Raghav';
 u1.displayName();
+u1.getNameLength();
+// }
--- app.ts
+++ app.ts
@@ -1,13 +1,20 @@
 class Users{
-    name:string="";
-    setName(name:string){
+    private name:string="";
+      setName(name:string){
         this.name=name;
     }
-    displayName(){
+      displayName(){
         console.log(this.name)
+        this.getNameLength()
     }
-}
+      getNameLength(){
+        console.log(this.name.length)
+    }
 
+}
 const u1= new Users();
 u1.setName("John");
+// u1.name='Raghav';
 u1.displayName();
+u1.getNameLength();
+// }
