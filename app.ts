b033c77c1a5f5767142b6f4d885feec25acf6183
Raghav Mittal <mittalraghav45@gmail.com>
2025-04-14 20:50:13 +0100

    constructor

--- app.ts
+++ app.ts
@@ -1,20 +1,27 @@
-class Users{
-    private name:string="";
-      setName(name:string){
-        this.name=name;
-    }
-      displayName(){
-        console.log(this.name)
-        this.getNameLength()
-    }
-      getNameLength(){
-        console.log(this.name.length)
-    }
+// class Users{
+//     private name:string="";
+//       setName(name:string){
+//         this.name=name;
+//     }
+//       displayName(){
+//         console.log(this.name)
+//         this.getNameLength()
+//     }
+//       getNameLength(){
+//         console.log(this.name.length)
+//     }
 
-}
-const u1= new Users();
-u1.setName("John");
-// u1.name='Raghav';
-u1.displayName();
-u1.getNameLength();
 // }
+// const u1= new Users();
+// u1.setName("John");
+// // u1.name='Raghav';
+// u1.displayName();
+// u1.getNameLength();
+// // }
+
+class Users{
+    constructor (){
+        console.log('constructor called')
+    }
+}
+const u1=new Users();
