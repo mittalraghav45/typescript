4fb2cc4b403976f53fb1fab4edc0764fedb18621
Raghav Mittal <mittalraghav45@gmail.com>
2025-04-14 19:53:52 +0100

    class

--- app.ts
+++ app.ts
@@ -4,7 +4,12 @@ class Users{
     {
         return `${user} is added`
     }
+
+    removeUser(user:string){
+        console.log(`${user} removed`)
+    }
 }
 
 let UserNew= new Users();
 console.log(UserNew.addUser('John Doe'));
+console.log(UserNew.removeUser('John Doe'));
